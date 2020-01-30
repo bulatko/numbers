cbbf71a2db3c6d96b06f4a3643db973e3a5970ec
bulatko <buloch03@yandex.ru>
2020-01-30 23:40:36 +0300

    Making Table class and auto filling of sheets

--- Table.php
+++ Table.php
@@ -1,7 +1,55 @@
 <?php
 
+require 'CONSTS.php';
+require_once __DIR__ . '/vendor/autoload.php';
 
 class Table
 {
+    public $id;
+    public function __construct()
+    {
+        global $SPREADSHEET_ID;
+        $this->id = $SPREADSHEET_ID;
+    }
+    public function find_numbers($operator, $type, $str){
+        $ret = [];
+        $googleAccountKeyFilePath = __DIR__ . '/assets/Phone numbers-9dc4bd6a8dd1.json';
+        putenv('GOOGLE_APPLICATION_CREDENTIALS=' . $googleAccountKeyFilePath);
+
+        $client = new Google_Client();
+        $client->useApplicationDefaultCredentials();
+
+        $client->addScope( 'https://www.googleapis.com/auth/spreadsheets' );
+
+        $service = new Google_Service_Sheets( $client );
+
+
+        $response = $service->spreadsheets->get($this->id);
+
+// Свойства таблицы
+        $spreadsheetProperties = $response->getProperties();
+        $spreadsheetProperties->title; // Название таблицы
+
+        foreach ($response->getSheets() as $sheet) {
+
+            // Свойства листа
+            $sheetProperties = $sheet->getProperties();
+            $sheetProperties->title; // Название листа
+
+            $gridProperties = $sheetProperties->getGridProperties();
+            $gridProperties->columnCount; // Количество колонок
+            $rows_num = $gridProperties->rowCount; // Количество строк
+
+        }
+
+        $range = "$operator $type!A1:B$rows_num";
+        $response = $service->spreadsheets_values->get($this->id, $range)->values;
+        foreach ($response as $item) {
+            if(stristr($item[0], $str))
+                $ret[] = [$item[0], $item[1]];
+        }
+
+        return $ret;
+    }
 
 }
--- example.php
+++ example.php
@@ -1,45 +1,33 @@
 <?php
 
+require 'CONSTS.php';
 require_once __DIR__ . '/vendor/autoload.php';
 $contains = $_GET['contains'];
-// Путь к файлу ключа сервисного аккаунта
 $googleAccountKeyFilePath = __DIR__ . '/assets/Phone numbers-9dc4bd6a8dd1.json';
 putenv('GOOGLE_APPLICATION_CREDENTIALS=' . $googleAccountKeyFilePath);
 
-// Документация https://developers.google.com/sheets/api/
 $client = new Google_Client();
 $client->useApplicationDefaultCredentials();
 
-// Области, к которым будет доступ
-// https://developers.google.com/identity/protocols/googlescopes
 $client->addScope( 'https://www.googleapis.com/auth/spreadsheets' );
 
 $service = new Google_Service_Sheets( $client );
 
-// ID таблицы
-$spreadsheetId = '1OeCT82snW7RBu037UraLWxNl_ziAbzXZ9GUAK1xZj6s';
+$spreadsheetId = $SPREADSHEET_ID;
 
-$response = $service->spreadsheets->get($spreadsheetId);
 
-// Свойства таблицы
-$spreadsheetProperties = $response->getProperties();
-$spreadsheetProperties->title; // Название таблицы
+$operator = ["МТС", "МЕГАФОН", "БИЛАЙН", "ТЕЛЕ2", "БЕЗЛИМИТ"];
+$type = ["БРОНЗА", "СЕРЕБРО", "ЗОЛОТО", "ПЛАТИНА", "БРИЛЛИАНТ"];
 
-foreach ($response->getSheets() as $sheet) {
-
-    // Свойства листа
-    $sheetProperties = $sheet->getProperties();
-    $sheetProperties->title; // Название листа
-
-    $gridProperties = $sheetProperties->getGridProperties();
-    $gridProperties->columnCount; // Количество колонок
-    $rows_num = $gridProperties->rowCount; // Количество строк
-
-}
-
-$range = "Лист1!A1:B$rows_num";
-$response = $service->spreadsheets_values->get($spreadsheetId, $range)->values;
-foreach ($response as $item) {
-    if(stristr($item[0], $contains))
-    echo $item[0] . " - " . $item[1] . "<BR>";
+for($i = 0; $i < 5; $i++){
+    for($j = 0; $j < 5; $j++){
+        $arr = [];
+        for($k = 0; $k < 1000; $k++){
+            $arr[] = [(string)(rand(89000000000, 89999999999)), (string)(rand(3, 20) * 1000)];
+        }
+        $sheet = $operator[$i] . " " . $type[$j] . "!A1";
+        $body    = new Google_Service_Sheets_ValueRange( [ 'values' => $arr ] );
+        $options = array( 'valueInputOption' => 'RAW' );
+        $service->spreadsheets_values->update( $spreadsheetId, $sheet, $body, $options );
+    }
 }
